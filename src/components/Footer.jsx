f3dfbcf2314c6e86632a4860d65545b8dfb15f36
Scott Cormier <chicormier@gmail.com>
2024-03-23 18:11:59 -0500

    adjustments to footer and errorpage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <footer>
+      <ul className='footer_categories'>
+        <li>
+          <Link to='citations/categories/:CCBY'>CC BY 4.0</Link>
+        </li>
+        <li>
+          <Link to='citations/categories/:CCBYSA'>CC BY-SA 4.0</Link>
+        </li>
+        <li>
+          <Link to='citations/categories/:CCBYNC'>CC BY-NC 4.0</Link>
+        </li>
+        <li>
+          <Link to='citations/categories/:CCBYNCSA'>CC BY-NC-SA 4.0</Link>
+        </li>
+        <li>
+          <Link to='citations/categories/:CCBYND'>CC BY-ND 4.0</Link>
+        </li>
+        <li>
+          <Link to='citations/categories/:CCBYNCND'>CC BY-NC-ND 4.0</Link>
+        </li>
+      </ul>
+      <div className='footer_copyright'>
+        <a href='https://creativecommons.org/share-your-work/cclicenses/'>
+          About CC Licenses
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
