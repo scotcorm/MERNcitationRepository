5c70ab05d81cfd6bfae07d737ff805fa97861a92
Scott Cormier <chicormier@gmail.com>
2024-04-07 15:41:38 -0500

    started linking frontend to backend

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,27 +4,27 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <footer>
-      <ul className='footer_categories'>
+      <ul className='footer_licenses'>
         <li>
-          <Link to='citations/categories/:CC0'>CC 0</Link>
+          <Link to='citations/licenses/:CC0'>CC 0</Link>
         </li>
         <li>
-          <Link to='citations/categories/:CCBY'>CC BY 4.0</Link>
+          <Link to='citations/licenses/:CCBY'>CC BY 4.0</Link>
         </li>
         <li>
-          <Link to='citations/categories/:CCBYSA'>CC BY-SA 4.0</Link>
+          <Link to='citations/licenses/:CCBYSA'>CC BY-SA 4.0</Link>
         </li>
         <li>
-          <Link to='citations/categories/:CCBYNC'>CC BY-NC 4.0</Link>
+          <Link to='citations/licenses/:CCBYNC'>CC BY-NC 4.0</Link>
         </li>
         <li>
-          <Link to='citations/categories/:CCBYNCSA'>CC BY-NC-SA 4.0</Link>
+          <Link to='citations/licenses/:CCBYNCSA'>CC BY-NC-SA 4.0</Link>
         </li>
         <li>
-          <Link to='citations/categories/:CCBYND'>CC BY-ND 4.0</Link>
+          <Link to='citations/licenses/:CCBYND'>CC BY-ND 4.0</Link>
         </li>
         <li>
-          <Link to='citations/categories/:CCBYNCND'>CC BY-NC-ND 4.0</Link>
+          <Link to='citations/licenses/:CCBYNCND'>CC BY-NC-ND 4.0</Link>
         </li>
       </ul>
       <div className='footer_copyright'>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+// import { registerUser } from '../../../server/controllers/userControllers';
 
 const Register = () => {
   const [userData, setUserData] = useState({
@@ -8,18 +10,40 @@ const Register = () => {
     password: '',
     password2: '',
   });
+  const [error, setError] = useState('');
+  const navigate = useNavigate;
 
   const changeInputHandler = (e) => {
     setUserData((prevState) => {
       return { ...prevState, [e.target.name]: e.target.value };
     });
   };
+
+  const registerUser = async (e) => {
+    e.preventDefault();
+    setError();
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_BASE_URL}/users/register`,
+        userData
+      );
+      const newUser = await response.data;
+      console.log(newUser);
+      if (!newUser) {
+        setError("Couldn't register user. Please try again.");
+      }
+      navigate('/');
+    } catch (err) {
+      setError(err.response.data.message);
+    }
+  };
+
   return (
     <section className='register'>
       <div className='container'>
         <h2>Sign Up</h2>
-        <form className='form register_form'>
-          <p className='form_error-message'>There was an error.</p>
+        <form className='form register_form' onSubmit={registerUser}>
+          {error && <p className='form_error-message'>{error}</p>}
           <input
             type='text'
             placeholder='Full Name'
